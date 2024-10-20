7e004067c5850354fc93c3be85490e67f889b37f
mjc <mjchavda121@gmail.com>
2024-10-20 15:54:08 +0530

    Practice code till 18th Oct

--- src/October/ex181024/Lab056.java
+++ src/October/ex181024/Lab056.java
@@ -5,5 +5,12 @@ public class Lab056 {
     int a =10;
         System.out.println(a++);
         System.out.println(--a);
+        System.out.println(a);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(++a);
+        System.out.println(++a);
+
+
     }
 }
